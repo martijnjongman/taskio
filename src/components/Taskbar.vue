636b52d049c24bbe3f67b15f2b7b0cd5a557bcd2
martijnjongman <martijnjongman9@gmail.com>
2020-12-13 16:07:22 +0100

    Add transaction animation

--- src/components/Taskbar.vue
+++ src/components/Taskbar.vue
@@ -3,11 +3,11 @@
         <form class="inputfield">
             <input type="text" name="task" class="input" placeholder="Add task">
 
-            <div class="add">
+            <button>
                 <svg class="cross" xmlns="http://www.w3.org/2000/svg" width="15.5" height="15.5" viewBox="0 0 15.5 15.5">
                     <path id="Icon_awesome-plus" data-name="Icon awesome-plus" d="M14.393,8.339H9.411V3.357A1.107,1.107,0,0,0,8.3,2.25H7.2A1.107,1.107,0,0,0,6.089,3.357V8.339H1.107A1.107,1.107,0,0,0,0,9.446v1.107a1.107,1.107,0,0,0,1.107,1.107H6.089v4.982A1.107,1.107,0,0,0,7.2,17.75H8.3a1.107,1.107,0,0,0,1.107-1.107V11.661h4.982A1.107,1.107,0,0,0,15.5,10.554V9.446A1.107,1.107,0,0,0,14.393,8.339Z" transform="translate(0 -2.25)" fill="#c4c4c4"/>
                 </svg>
-            </div>
+            </button>
         </form>
     </div>
 </template>
@@ -35,13 +35,21 @@
                 font-weight: 600;
                 font-size: clamp(0.8rem, 1.25vw, 1.5rem);
             }
-            .add{
+            button{
+                background: none;
+                border-radius: 5px;
+                border: none;
                 display: flex;
                 align-items: center;
-                padding-right: clamp(0.75rem, 2vw, 2rem);
+                cursor: pointer;
+                padding: 0 clamp(0.75rem, 2vw, 2rem);
                 .cross{
                     width: clamp(15px, 2vw, 25px);
                     height: clamp(15px, 2vw, 25px);
+                    :hover{
+                        fill: #737d8f;
+                        transition: .3s ease-in-out;
+                    }                 
                 }
             }
         }
