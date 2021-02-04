216f7f85d6d5c5bf4fb235b0b1ecd3e50f15e1e8
martijnjongman <martijnjongman9@gmail.com>
2021-02-04 15:10:11 +0100

    Add pwa function

---
+++ dist/precache-manifest.fe5a950c565527ddfb7a2b7ab1118b04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95cad350cf284f12ef84",
+    "url": "/taskio/css/app.085f24bc.css"
+  },
+  {
+    "revision": "e2a9c5581475f7feede0910994faeefa",
+    "url": "/taskio/index.html"
+  },
+  {
+    "revision": "95cad350cf284f12ef84",
+    "url": "/taskio/js/app.cb2f3c39.js"
+  },
+  {
+    "revision": "29ee6f301462639fb8d2",
+    "url": "/taskio/js/chunk-vendors.bbf5b751.js"
+  },
+  {
+    "revision": "aae7b7bf33064ed4f271cacbbc7daf2a",
+    "url": "/taskio/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/taskio/robots.txt"
+  }
+]);
